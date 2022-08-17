27d0774b104172a2f03c8b5c134f15ac1b28bad3
UnknowEntity <theplayer357@gmail.com>
2022-08-17 22:55:56 +0700

    Refactor code test and add more case

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello Chris"
+	t.Run("Say Hello to People", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello Chris"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("Say Hello World when omit name", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
